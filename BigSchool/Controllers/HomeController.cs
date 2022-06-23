dc391a3857ef5ccbbbb8917285d0e0dcf523cca4
KDV-it <voduykhang312001@gmail.com>
2022-06-24 02:27:19 +0700

    Add project files.

---
+++ BigSchool/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using BigSchool.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+
+namespace BigSchool.Controllers
+{
+    public class HomeController : Controller
+    {
+        private ApplicationDbContext _dbContext;
+        public HomeController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+        public ActionResult Index()
+        {
+            var upcommingCourses = _dbContext.Courses
+                .Include(c => c.Lecturer)
+                .Include(c => c.Category)
+                .Where(c => c.DateTime > DateTime.Now);
+            return View(upcommingCourses);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
